63f884e5f245f36b7b081e1a3e8401ee249f2e8b
milanzagorac <zagoracm@gmail.com>
2021-07-18 14:59:08 -0400

    routes working

--- config.js
+++ config.js
@@ -1,13 +1,9 @@
 /** Common config for bookstore. */
 
-
-let DB_URI = `postgresql://`;
-
 if (process.env.NODE_ENV === "test") {
-  DB_URI = `${DB_URI}/books-test`;
+    DB_URI = `postgresql:///books-test`;
 } else {
-  DB_URI = process.env.DATABASE_URL || `${DB_URI}/books`;
+    DB_URI = process.env.DATABASE_URL || `postgresql:///books`;
 }
-
 
 module.exports = { DB_URI };
